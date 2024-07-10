0776710dd2ab24ad14e02bf05f112ccfc328d6cf
Tony Lin <tonyelin@gmail.com>
2024-07-10 17:35:44 +0800

    add try/except block in extract_table()

--- fda_nme/scrape.py
+++ fda_nme/scrape.py
@@ -82,24 +82,28 @@ class Extract:
         :return:
         """
         # The information is organized across multiple pages. Tables will need to be generated, processed, and concated.
-        tables = tabula.read_pdf(url, pages='all', lattice=True, pandas_options={"header": [0, 1]},
-                                 multiple_tables=True)
+        try:
+            tables = tabula.read_pdf(url, pages='all', lattice=True, pandas_options={"header": [0, 1]},
+                                     multiple_tables=True)
 
-        df = pd.concat(tables[1:], ignore_index=True)  # Do not use table[0] becuase it is junk
-        data_df = df.replace(to_replace=r'\r', value=' ', regex=True)  # replace the '\r' string with a space
+            df = pd.concat(tables[1:], ignore_index=True)  # Do not use table[0] becuase it is junk
+            data_df = df.replace(to_replace=r'\r', value=' ', regex=True)  # replace the '\r' string with a space
 
-        header = data_df.iloc[0]  # Grab the first row for the header
-        data_df = data_df[1:]  # drop header column
-        data_df.columns = header
+            header = data_df.iloc[0]  # Grab the first row for the header
+            data_df = data_df[1:]  # drop header column
+            data_df.columns = header
 
-        # Drop row for BLA header
-        data_df = data_df.drop(data_df[data_df['APPLICATION NUMBER'] == 'BLA NUMBER'].index)
+            # Drop row for BLA header
+            data_df = data_df.drop(data_df[data_df['APPLICATION NUMBER'] == 'BLA NUMBER'].index)
 
-        # Drop columns or rows where all elements are NaN
-        data_df = data_df.dropna(axis=1, how='all')
-        data_df = data_df.dropna(axis=0, how='all')
+            # Drop columns or rows where all elements are NaN
+            data_df = data_df.dropna(axis=1, how='all')
+            data_df = data_df.dropna(axis=0, how='all')
 
-        return data_df
+            return data_df
+
+        except:
+            print("There is an exception!")
 
     def get_current_year(self, url: str = None):
         """
