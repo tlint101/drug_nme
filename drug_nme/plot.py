bdd4cd9016dbefa069904ca803a3aeb360981223
Tony Lin <tonyelin@gmail.com>
2024-07-22 16:35:43 +0800

    add initializer params

--- drug_nme/plot.py
+++ drug_nme/plot.py
@@ -9,5 +9,16 @@ __all__ = ["Plot"]
 
 
 class Plot:
-    def __init__(self, df):
+    def __init__(self, df: pd.DataFrame = None, source: str = None):
+        """
+        Initialize the Plot object. Need to indicate where the source is from. Either from openFDA or from Guide to
+        Pharmacology.
+        """
         self.df = df
+        self.source = source
+
+        if source is None:
+            raise ValueError("Data source must be 'fda' or 'pharmacology'!")
+
+    def bar(self, df, source):
+        pass
