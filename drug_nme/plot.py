0b250041eec33469aff48768220055cc39048932
Tony Lin <tonyelin@gmail.com>
2024-07-24 09:55:18 +0800

    add documentation

--- drug_nme/plot.py
+++ drug_nme/plot.py
@@ -9,16 +9,24 @@ __all__ = ["Plot"]
 
 
 class Plot:
-    def __init__(self, df: pd.DataFrame = None, source: str = None):
+    def __init__(self, df: pd.DataFrame = None, type_col: str = None):
         """
         Initialize the Plot object. Need to indicate where the source is from. Either from openFDA or from Guide to
         Pharmacology.
-        """
-        self.df = df
-        self.source = source
 
-        if source is None:
-            raise ValueError("Data source must be 'fda' or 'pharmacology'!")
+        :param df: pd.DataFrame
+            Input pd.DataFrame containing drug approvals. The DataFrame must be obtained from the DataFetcher or Scrape
+            classes.
+        :param type_col: str
+            The name of the column for processing. Name should match that of the existing column headers from the
+            pd.DataFrame.
+        """
+
+        self.df = df
+        self.source = type_col;
+
+        # if type_col is None:
+        #     raise ValueError("Data source must be 'fda' or 'pharmacology'!")
 
     def bar(self, df, source):
         pass
