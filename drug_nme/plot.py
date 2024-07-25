f4ffeded73cae073b3385a00099262781d28e31d
Tony Lin <tonyelin@gmail.com>
2024-07-25 17:33:21 +0800

    function for donut()

--- drug_nme/plot.py
+++ drug_nme/plot.py
@@ -12,6 +12,7 @@ __all__ = ["Plot"]
 # globally remove grid lines from plot
 plt.rcParams['axes.grid'] = False
 
+
 class Plot:
     def __init__(self, df: pd.DataFrame = None, sort_col: str or list = None):
         """
@@ -105,6 +106,31 @@ class Plot:
                 figsize: tuple[float, float] = (10, 5),
                 savepath: str = None
                 ):
+        """
+        :param data: pd.DataFrame
+            Input query pd.DataFrame. Should be processed. If not given, function will utilize the initialized processed
+            pd.DataFrame.
+        :param x: str
+            The column header for the X-axis.
+        :param y: str
+            The column header for the Y-axis.
+        :param groups: str
+            The column header to set the hue.
+        :param title: str
+            Set the title of the plot.
+        :param label: bool
+            Determine annotations on the stacked bar chart.
+        :param color_palette: str or list
+            Set the color palette for the plot. Can be single palette name or a list of color names or hex codes.
+        :param fontsize: int
+            Set the fontsize for the annotations.
+        :param legend_loc: str
+            Set the legend location for the plot.
+        :param figsize: tuple
+            Set the size of the figure.
+        :param savepath: str
+            Set the save location for the plot.
+        """
         if data is None:
             data = self.df
 
@@ -142,8 +168,80 @@ class Plot:
             image.legend_.remove()
             legend(loc=legend_loc)
 
-
         # save fig
+        if savepath:
+            # adjust layout to prevent clipping
+            plt.tight_layout()
+            plt.savefig(savepath, dpi=300)
+            plt.close()
+
+        return image
+
+    def donut(self,
+              data: pd.DataFrame = None,
+              title: str = None,
+              color_palette: str or list = None,
+              pctdistance: int = 0.8,
+              labeldistance: int = 1.1,
+              fontsize: int = 10,
+              legend_loc: str = None,
+              figsize: tuple[float, float] = (10, 5),
+              savepath: str = None
+              ):
+        """
+        :param data: pd.DataFrame
+            Input query pd.DataFrame. Should be processed. Function can only accept data that has been sliced by year.
+        :param title: str
+            Set the title of the plot.
+        :param color_palette: str or list
+            Set the color palette for the plot.
+        :param pctdistance: int
+            Set the position of the percentage labels.
+        :param labeldistance: int
+            Set the position of the category labels.
+        :param fontsize: int
+            Set the fontsize for the annotations.
+        :param legend_loc: str
+            Set the positions of the figure ligend.
+        :param figsize: tuple
+            Set the size of the figure.
+        :param savepath: str
+            Set the save location for the plot.
+        """
+        # Set Seaborn color palette
+        color_palette = sns.color_palette(color_palette)
+
+        plt.figure(figsize=figsize)
+
+        # Plot the pie chart
+        wedges, texts, autotexts = plt.pie(
+            data['Count'],
+            labels=data['type'],
+            startangle=90,
+            colors=color_palette,
+            autopct=lambda pct: f'{int(pct / 100. * sum(data['Count']))}\n({pct:.1f}%)',
+            wedgeprops=dict(width=0.4),  # Donut hole size
+            textprops={'color': 'black'},
+            pctdistance=pctdistance,
+            labeldistance=labeldistance
+        )
+
+        for text in autotexts:
+            text.set_fontsize(fontsize)
+
+        # Draw a circle at the center of the plot
+        image = plt.Circle((0, 0), 0.6, color='white')  # Smaller radius for a smaller hole
+        plt.gca().add_artist(image)
+
+        # Equal aspect ratio ensures that pie is drawn as a circle
+        plt.axis('equal')
+
+        if legend_loc:
+            legend(loc=legend_loc)
+
+        if title:
+            plt.title(title)
+
         if savepath:
             # adjust layout to prevent clipping
             plt.tight_layout()
