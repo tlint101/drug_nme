1eb06575ad58aea9348d39cbefcb8c77408ae6af
Tony Lin <tonyelin@gmail.com>
2025-03-04 11:47:32 +0800

    Add methods for get_data() and get_gene_id()

--- drug_nme/target.py
+++ drug_nme/target.py
@@ -6,6 +6,7 @@ import requests
 import pandas as pd
 from tqdm import tqdm
 from typing import Union, Optional
+from drug_nme.utils import GtoP, uniprot_query
 
 __all__ = ["Target"]
 
@@ -17,11 +18,17 @@ class Target:
             Set the UniprotID for target query.
         """
         # set link to Guide To Pharmacology
-        self.GTOPDB = 'https://www.guidetopharmacology.org/services/'
+        self.GTOPDB = GtoP
         self.uniprot = uniprot_id
 
     def get_data(self, uniprot_id: Optional[Union[str, list]] = None):
-
+        """
+        Get information for a protein target by their Uniprot ID. This will give a table containing their accession,
+        source database, target_id for the Guide to Pharmacology API, protein type and protein target name. The protein
+        target name will match the targe gene.
+        :param uniprot_id: Union[str, list]
+            Get gene id for a protein by their Uniprot ID.
+        """
         # check instance variable
         if uniprot_id is None:
             uniprot_id = self.uniprot
@@ -31,8 +38,17 @@ class Target:
         # if input is a list, loop
         if isinstance(uniprot_id, list):
             dfs = []
-            for id in tqdm(uniprot_id, desc=f'Getting data'):
+            for id in tqdm(uniprot_id, desc=f'Getting Target Data'):
                 target_id, target_type, target_name = self._get_target_id_by_uniprot_id(id)
+
+                # if there is no target_name
+                if target_name == "" or target_name is None:
+                    target_name = self.get_gene_id(id)
+                    # print('New target name:', target_name)
+
+                # # for troubleshooting
+                # print(f"Target Name: {target_name}")
+
                 pull_data = self._get_data_by_target_id(target_id, target_type, target_name)
                 dfs.append(pull_data)
 
@@ -41,11 +57,60 @@ class Target:
 
         else:
             target_id, target_type, target_name = self._get_target_id_by_uniprot_id(uniprot_id)
-            print(f"target_id {target_id}, \ntarget_type {target_type}, \ntarget_name {target_name}")
+
+            # if there is no target_name
+            if target_name == "":
+                target_name = self.get_gene_id(uniprot_id)
+
+                # # for troubleshooting
+                # print('New target name:', target_name)
+
             pull_data = self._get_data_by_target_id(target_id, target_type, target_name)
             data = pull_data
 
         return data
+
+    def get_gene_id(self, uniprot_id: Optional[Union[str, list]] = None):
+        """
+        Get gene of protein using protein Uniprot ID.
+        :param uniprot_id: Optional[Union[str, list]}
+            Get gene id for a protein by their Uniprot ID.
+        """
+        if uniprot_id is None:
+            uniprot_id = self.uniprot
+        if self.uniprot is None and uniprot_id is None:
+            raise AttributeError("You must specify a target Uniprot ID!")
+
+        # if input is a list, loop
+        if isinstance(uniprot_id, list):
+            ids = {}
+            for id in tqdm(uniprot_id, desc=f'Getting Target Gene ID'):
+                # query uniprot rest
+                url = uniprot_query + f"{id}"
+                response = requests.get(url)
+
+                # pul data
+                if response.status_code == 200:
+                    data = response.json()
+                    # get gene name
+                    gene_name = data.get("genes", [{}])[0].get("geneName", {}).get("value", None)
+                    ids[id] = gene_name
+                elif response.status_code == 400:
+                    print(f"Error: Failed to get data for Uniprot ID: {id}!!")
+            return ids
+        else:
+            # query uniprot rest
+            url = uniprot_query + f"{uniprot_id}"
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                data = response.json()
+                # get gene name
+                gene_name = data.get("genes", [{}])[0].get("geneName", {}).get("value", None)
+
+                return gene_name
+            else:
+                return print(f"Error: Failed to get data for {uniprot_id}!!")
 
     """Support functions"""
 
@@ -65,16 +130,12 @@ class Target:
             target_type = only_item["type"]
             target_name = only_item["abbreviation"]
             return target_id, target_type, target_name
-        else:
-            target_id = target_data["targetId"]
-            target_type = target_data["type"]
-            target_name = target_data["abbreviation"]
 
-        return target_id, target_type, target_name
+        return None
 
     def _get_data_by_target_id(self, target_id, target_type, target_name):
         """
-        
+        Get data from Guide to Pharmacology API and place it in a dataframe.
         """
         url = f"{self.GTOPDB}/targets/{target_id}/databaseLinks?species=Human"
         response = requests.get(url)
